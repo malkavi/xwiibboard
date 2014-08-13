d664c6ac2ab5d4ae4d7623248a8546d8a2d005b3
malkavi <cain1986@gmail.com>
2014-08-13 11:42:39 +0200

    added sql file and gitignore

---
+++ mysql.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlite3 as lite
+import sys
+
+def guardar_peso(peso):
+    con = lite.connect('fitbit.db')
+
+    with con:
+
+        cur = con.cursor()
+        #weight table
+        cur.execute("CREATE TABLE IF NOT EXISTS weight(CURRENT_DATE DATETIME PRIMARY KEY NOT NULL  DEFAULT (CURRENT_DATE), weight REAL)")
+        cur.execute("INSERT OR REPLACE INTO weight (CURRENT_DATE, weight) VALUES (DATE('now'), "+str(peso)+")")
